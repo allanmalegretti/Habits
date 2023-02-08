4f19d4c4e5d556b9207e85210766a6a4ef69011b
Allan M Alegretti <allanalegretti@gmail.com>
2023-02-08 09:45:33 -0300

    Projeto finalizado

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 const form = document.querySelector('#form-habits');
 const nlwSetup = new NLWSetup(form);
+const button = document.querySelector('header button');
 
-const data = { 
-    run: ['01-01', '01-02', '01-06'], 
-    water: ['01-04', '01-05'],
-    food: ['01-01', '01-03'],
-    journal: ['01-02', '01-03', '01-06'],
-    takePills: ['01-03', '01-04', '01-05'],
-  }
-  
-  nlwSetup.setData(data);
-  nlwSetup.load();
+button.addEventListener("click", add);
+form.addEventListener("change", save);
+
+function add(){
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5);
+    const dayExists = nlwSetup.dayExists(today);
+
+    if (dayExists){
+        alert("Dia ja incluido");
+        return;
+    }
+
+    alert("Dia incluido com sucesso");
+    nlwSetup.addDay(today);
+}
+
+function save(){
+    console.log(nlwSetup.data);
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data));
+}
+
+// const data = { 
+//     run: ['01-01', '01-02', '01-06'], 
+//     water: ['01-04', '01-05'],
+//     food: ['01-01', '01-03'],
+//     journal: ['01-02', '01-03', '01-06'],
+//     takePills: ['01-03', '01-04', '01-05'],
+//   }
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {};
+nlwSetup.setData(data);
+nlwSetup.load();
